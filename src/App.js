61cbdbb8c2afc2542fa83ac7ce8cbd929f3c3538
gopal-sharma <gopalsharmabsa@gmail.com>
2020-02-03 23:16:57 +0530

    authentication in react graphql

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+//import ApolloClient from 'apollo-boost';
+
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import './App.css';
 import BookList from './Components/BookList';
 import AddBook from './Components/AddBook';
 
-const client=new ApolloClient({
-  uri:'http://localhost:4000/graphql'
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
 });
+
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  console.log(token,"tokennnnnnnnnnnnnnn");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
  
   return (
